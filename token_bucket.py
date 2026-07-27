38039dad5067f96f11c70cc4f150d5b29acfb86a
Naman Dadhich <ndmajor2005@gmail.com>
2026-07-28 03:34:50 +0530

    Add Token_Bucket class with initialization for bucket capacity and refill rate

--- token_bucket.py
+++ token_bucket.py
@@ -1,4 +1,9 @@
 from base import BaseClass
 
 class Token_Bucket(BaseClass):
-    pass
+    bucket_capacity:int
+    refill_rate:float
+    def __init__(self,bucket_capacity,refill_rate):
+        self.bucket_capacity=bucket_capacity
+        self.refill_rate=refill_rate
+       
