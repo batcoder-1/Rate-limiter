26eff3999aabcd660943386387d7978d234bbb2b
Naman Dadhich <ndmajor2005@gmail.com>
2026-08-23 15:14:58 +0530

    Add .gitignore file and refactor token_bucket.lua for local variable usage

--- scripts/token_bucket.lua
+++ scripts/token_bucket.lua
@@ -1,21 +1,20 @@
-user_id=KEYS[1] -- use hset and hmget or hashing as we are going to add ttl for inactive users
-bucket_capacity=tonumber(ARGV[1]) 
-refill_rate=tonumber(ARGV[2])
-time=redis.call("TIME")
-current_timestamp=time[1]
-response=redis.call("GET",user_id)
+local user_id=KEYS[1] -- use hset and hmget or hashing as we are going to add ttl for inactive users
+local bucket_capacity=tonumber(ARGV[1]) 
+local refill_rate=tonumber(ARGV[2])
+local time=redis.call("TIME")
+local current_timestamp=time[1]
+local response=redis.call("GET",user_id..":current_tokens")
 
 if response == false then
     redis.call("MSET",user_id..":current_tokens",bucket_capacity-1,
                 user_id..":last_timestamp",current_timestamp)
     return true
 else
-    current_tokens=redis.call("GET",user_id..":current_tokens")
-    last_timestamp=redis.call("GET",user_id..":last_timestamp")
+    local current_tokens=tonumber(redis.call("GET",user_id..":current_tokens"))
+    local last_timestamp=tonumber(redis.call("GET",user_id..":last_timestamp"))
     current_tokens=math.min(bucket_capacity,(current_timestamp-last_timestamp)*refill_rate+current_tokens)
     if current_tokens>=1 then 
-        current_tokens=current_tokens-1
-        redis.call("MSET",user_id..":current_tokens",current_tokens,
+        redis.call("MSET",user_id..":current_tokens",current_tokens-1,
                 user_id..":last_timestamp",current_timestamp)
         return true
     end
