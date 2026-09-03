2cca71929ae5ada2b01389a609092489202a8837
Naman Dadhich <ndmajor2005@gmail.com>
2026-09-03 12:09:13 +0530

    Refactor token management to use HSET for current tokens and last timestamp

--- scripts/token_bucket.lua
+++ scripts/token_bucket.lua
@@ -1,24 +1,30 @@
-local user_id=KEYS[1] -- use hset and hmget or hashing as we are going to add ttl for inactive users
+local user_id=KEYS[1]
 local bucket_capacity=tonumber(ARGV[1]) 
 local refill_rate=tonumber(ARGV[2])
 local time=redis.call("TIME")
-local current_timestamp=time[1]
-local response=redis.call("GET",user_id..":current_tokens")
+local current_timestamp=time[1]+time[2]/1000000
+local response=redis.call("HGET",user_id,"current_tokens")
+local ttl_time
+if refill_rate == 0 then
+    ttl_time=bucket_capacity
 
-if response == false then
-    redis.call("MSET",user_id..":current_tokens",bucket_capacity-1,
-                user_id..":last_timestamp",current_timestamp)
+else
+    ttl_time=math.ceil(bucket_capacity/refill_rate)+5
+end
+    if response == false then
+    redis.call("HSET",user_id,"current_tokens",bucket_capacity-1,"last_timestamp",current_timestamp)
+    redis.call("EXPIRE",user_id,ttl_time)
     return true
 else
-    local current_tokens=tonumber(redis.call("GET",user_id..":current_tokens"))
-    local last_timestamp=tonumber(redis.call("GET",user_id..":last_timestamp"))
+    local current_tokens=tonumber(redis.call("HGET",user_id,"current_tokens"))
+    local last_timestamp=tonumber(redis.call("HGET",user_id,"last_timestamp"))
     current_tokens=math.min(bucket_capacity,(current_timestamp-last_timestamp)*refill_rate+current_tokens)
     if current_tokens>=1 then 
-        redis.call("MSET",user_id..":current_tokens",current_tokens-1,
-                user_id..":last_timestamp",current_timestamp)
+        redis.call("HSET",user_id,"current_tokens",current_tokens-1,"last_timestamp",current_timestamp)
+        redis.call("EXPIRE",user_id,ttl_time)
         return true
     end
-      redis.call("MSET",user_id..":current_tokens",current_tokens,
-                user_id..":last_timestamp",current_timestamp)
+      redis.call("HSET",user_id,"current_tokens",current_tokens,"last_timestamp",current_timestamp)
+      redis.call("EXPIRE",user_id,ttl_time)
 end
     return false
