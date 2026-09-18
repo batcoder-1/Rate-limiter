e6668817042867c64fcd24d1aefbfb21dc79b0fa
Naman Dadhich <ndmajor2005@gmail.com>
2026-09-18 16:58:53 +0530

    Refactor Leaky Bucket initialization and enhance Lua script for capacity management

--- leaky_bucket.py
+++ leaky_bucket.py
@@ -5,12 +5,12 @@ script=(base_dir/"scripts"/"leaky_bucket.lua").read_text()
 class Leaky_Bucket(BaseClass):
   capacity_of_bucket:int
   leakage_rate:int
-  def __init__(self,redis_client,nameSpace,capacity,rate):
-    if rate == 0:
-      raise ValueError("leakage rate cannot be zero")
+  def __init__(self,redis_client,nameSpace,bucket_capacity,leakage_rate):
+    if leakage_rate <= 0 or bucket_capacity <= 0:
+      raise ValueError("leakage rate and capacity cannot be zero")
     super().__init__(redis_client,nameSpace)
-    self.capacity_of_bucket=capacity
-    self.leakage_rate=rate
+    self.capacity_of_bucket=bucket_capacity
+    self.leakage_rate=leakage_rate
     self.script=self.redis_client.register_script(script)
   def isAllow(self, user_id):
     allowed=self.script(
--- scripts/leaky_bucket.lua
+++ scripts/leaky_bucket.lua
@@ -4,3 +4,22 @@ local leakage_rate=tonumber(ARGV[2])
 local time=redis.call("TIME")
 local current_timestamp=time[1]+time[2]/1000000
 local response=redis.call("HGET",user,"current_capacity")
+local ttl_time=math.ceil(bucket_capacity/leakage_rate)+5
+
+if response == false then
+    redis.call("HSET",user,"current_capacity",1,"last_timestamp",current_timestamp)
+    redis.call("EXPIRE",user,ttl_time)
+    return true
+else
+    local current_capacity=tonumber(redis.call("HGET",user,"current_capacity"))
+    local last_timestamp=tonumber(redis.call("HGET",user,"last_timestamp"))
+    current_capacity=math.max(0,current_capacity-(current_timestamp-last_timestamp)*leakage_rate)
+    if current_capacity < bucket_capacity then
+        redis.call("HSET",user,"current_capacity",current_capacity+1,"last_timestamp",current_timestamp)
+        redis.call("EXPIRE",user,ttl_time)
+        return true
+    end
+    redis.call("HSET",user,"current_capacity",current_capacity,"last_timestamp",current_timestamp)
+    redis.call("EXPIRE",user,ttl_time)
+end
+return false
